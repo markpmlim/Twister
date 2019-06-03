33364790ebae6662f887993bafe6fbb0c3226e52
markpmlim <markpmlim@users.noreply.github.com>
2019-06-03 15:08:29 +0800

    Update Contents.swift

--- Twister.playground/Contents.swift
+++ Twister.playground/Contents.swift
@@ -1,7 +1,7 @@
 // A playground can be used to test geometry shader modifiers quickly.
 // To demonstrate a twisting effect using a geometry shader modifier
 // https://www.objc.io/issues/18-games/scenekit/#extending-the-default-rendering
-// Original code must be modified slightly.
+// An explicit animation of the rendered object is now working correctly.
 // Both SCNGeometry and SCNMaterial conforms to the SCNAnimatable protocol.
 // Requires: XCode 8.3.2 macOS Sierra or later
 
@@ -35,16 +35,14 @@ sceneView.allowsCameraControl = true
 sceneView.showsStatistics = true
 
 // Geometry - Metal will translate the OpenGL code.
-// The uniform "twistFactor" must be assigned a value.
-// Its value can't be assigned using the setValue:forKey method.
 let geometryShaderModifier =
-    "uniform float twistFactor = 1.0;\n" +
+    "uniform float twistFactor;\n" +
     "mat4 rotationAroundX(float angle) {\n" +
-    "return mat4(1.0,    0.0,         0.0,        0.0, \n" +
-    "            0.0,    cos(angle), -sin(angle), 0.0, \n" +
-    "            0.0,    sin(angle),  cos(angle), 0.0, \n" +
-    "            0.0,    0.0,         0.0,        1.0); \n" +
-    "} \n" +
+    "return mat4(1.0,    0.0,         0.0,        0.0,\n" +
+    "            0.0,    cos(angle), -sin(angle), 0.0,\n" +
+    "            0.0,    sin(angle),  cos(angle), 0.0,\n" +
+    "            0.0,    0.0,         0.0,        1.0);\n" +
+    "}\n" +
     "#pragma body\n" +
     "float rotationAngle = _geometry.position.x * twistFactor;\n" +
     "mat4 rotationMatrix = rotationAroundX(rotationAngle);\n" +
@@ -55,11 +53,15 @@ let geometryShaderModifier =
 // Attached the shader modifier to the torus' geometry
 torus.shaderModifiers = [.geometry : geometryShaderModifier]
 
+// The uniform "twistFactor" must be assigned a value.
+torus.setValue(5.0, forKey: "twistFactor")
+
 // CABasicAnimation is a sub-class of CAAnimation which conforms to SCNAnimationProtocol
 // Check: Is the keyPath "twistFactor" KVC?
-// Yes, SceneKit binds the values of shader variables using KVO.
+// Yes, SceneKit binds values of shader variables using KVO.
 // Custom uniforms can be animated using explicit animations.
-// Using the key path "twistFactor" doesn't work.
+// There is no animation if the key path is "twistFactor".
+// Ref. Animating SceneKit Content - Explicitly Create an Animation
 let twistAnimation = CABasicAnimation(keyPath: "geometry.twistFactor")
 twistAnimation.fromValue = NSNumber(value: 5.0)
 twistAnimation.toValue   = NSNumber(value: 0.0)
@@ -68,7 +70,8 @@ twistAnimation.autoreverses  = true
 twistAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
 twistAnimation.repeatCount = .greatestFiniteMagnitude
 
-torusNode.addAnimation(twistAnimation,
-                       forKey: "Twist the torus")
+// No animation if the animation is attached to "torus" (an instance of SCNGeometry)
+// even though the class SCNGeometry conforms to SCNAnimationProtocol
+torusNode.addAnimation(twistAnimation, forKey: "Twist the torus")
 
 PlaygroundPage.current.liveView = sceneView
